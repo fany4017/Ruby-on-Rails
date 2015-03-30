f0f132079fd8def900da9397e42db3ddb849aea0
brian <fany4017@naver.com>
2015-03-30 12:38:14 +0000

    댓글 달기 기능 추가하기

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-	belong_to :user
-	belong_to :post
-	validates : content, :presence => {:message => "내용을 반드시 입력하셔야 합니다."}
+	belongs_to :user
+	belongs_to :post
+	validates :content, :presence => { :message => "내용을 반드시 입력하셔야 합니다." }
 end

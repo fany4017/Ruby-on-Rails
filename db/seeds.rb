c1424aef35748d21213e286361637064809de26f
brian <fany4017@naver.com>
2015-03-30 04:35:23 +0000

    Post 테이블의 시드 데이터 입력하기

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = ["한식","일식","중식","양식"]
+
+categories.each do |category|
+	0.upto(1) do |i|
+		p = Post.new
+		p.user_id = i + 1
+		p.category = category
+		p.title = "#{category}집#{i} 이름"
+		p.content = "#{category}집#{i} 설명"
+		p.save
+	end
+end
